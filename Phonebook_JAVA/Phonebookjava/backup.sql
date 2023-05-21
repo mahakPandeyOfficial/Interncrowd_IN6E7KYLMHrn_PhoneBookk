5a04eded1ccac6a5b405a6d83601d963251278f5
Mahak <mahak0312pandey@gmail.com>
2023-05-21 16:35:47 +0530

    Task 2 Phone Book Mini project by Intern Crowd

---
+++ Phonebook_JAVA/Phonebookjava/backup.sql
@@ -0,0 +1,4 @@
+insert into notebook.table2 values ('brunoo' , 'espp' , '5201110020');
+insert into notebook.table2 values ('henry' , 'ghts' , '7537890000');
+insert into notebook.table2 values ('harry' , 'xyzz' , '7609998500');
+insert into notebook.table2 values ('christine' , 'abcd' , '8520001540');
